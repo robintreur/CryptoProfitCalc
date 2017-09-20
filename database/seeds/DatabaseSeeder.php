069d0f780cb3cfa74965cf3e265d3b6d8c76ee59
robintreur <robintreur@gmail.com>
2017-09-20 11:05:33 +0200

    Seeded first 300 cryptocoins to database table 'coins' with DatabaseSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//         $this->call(UsersTableSeeder::class);
+
+        $str = file_get_contents('https://api.coinmarketcap.com/v1/ticker/?limit=300&convert=EUR');
+        $coins = json_decode($str, true);
+
+        foreach ($coins as $coin) {
+            DB::table('coins')->insert([
+                'name' => $coin['name'],
+                'short_name' => $coin['symbol'],
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ]);
+        }
     }
 }
